7dc7e15712d342bed1364e8549e9e3db92512a68
DJay <digs.chauhan@gmail.com>
2023-06-10 19:52:08 -0700

    added eks cluster code

--- live/main.tf
+++ live/main.tf
@@ -4,3 +4,13 @@ module "vpc" {
 
   vpc_name = "cluster_vpc"
 }
+
+module "eks" {
+  source = "../modules/eks"
+
+  cluster_name          = "dev-cluster"
+  vpc_id                = module.vpc.cluster_vpc_id
+  private_subnet_ids    = module.vpc.private_subnet_ids
+  public_subnet_ids     = module.vpc.public_subnet_ids
+  workers_instance_type = "t2.micro"
+}
--- live/outputs.tf
+++ live/outputs.tf
@@ -17,3 +17,31 @@ output "internet_gateway" {
 output "nat_gateway" {
   value = module.vpc.nat_gateway
 }
+
+output "eks_security_group" {
+  value = module.eks.eks_security_group
+}
+
+output "eks_ca" {
+  value = module.eks.eks_ca
+}
+
+output "eks_endpoint" {
+  value = module.eks.eks_endpoint
+}
+
+output "eks_sg" {
+  value = module.eks.eks_sg
+}
+
+output "eks_cluster_role_arn" {
+  value = module.eks.eks_cluster_role_arn
+}
+
+output "workers_role_arn" {
+  value = module.eks.workers_role_arn
+}
+
+output "workers_sg" {
+  value = module.eks.workers_sg
+}
